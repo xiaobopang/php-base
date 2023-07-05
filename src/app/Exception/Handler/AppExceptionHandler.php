de125d1d6c79eb63059b264deb391839c2b29502
pangxiaobo <pangxiaobo@vchangyi.com>
2023-07-05 13:54:50 +0800

    exception update

--- src/app/Exception/Handler/AppExceptionHandler.php
+++ src/app/Exception/Handler/AppExceptionHandler.php
@@ -11,14 +11,20 @@ declare(strict_types=1);
  */
 namespace App\Exception\Handler;
 
+use App\Exception\BusinessException;
 use Hyperf\Contract\StdoutLoggerInterface;
+use Hyperf\Di\Annotation\Inject;
 use Hyperf\ExceptionHandler\ExceptionHandler;
 use Hyperf\HttpMessage\Stream\SwooleStream;
 use Psr\Http\Message\ResponseInterface;
 use Throwable;
+use Hyperf\HttpServer\Contract\ResponseInterface as HttpResponse;
 
 class AppExceptionHandler extends ExceptionHandler
 {
+    #[Inject]
+    protected HttpResponse $httpResponse;
+
     public function __construct(protected StdoutLoggerInterface $logger)
     {
     }
@@ -27,7 +33,16 @@ class AppExceptionHandler extends ExceptionHandler
     {
         $this->logger->error(sprintf('%s[%s] in %s', $throwable->getMessage(), $throwable->getLine(), $throwable->getFile()));
         $this->logger->error($throwable->getTraceAsString());
-        return $response->withHeader('Server', 'Hyperf')->withStatus(500)->withBody(new SwooleStream('Internal Server Error.'));
+
+        // 判断被捕获到的异常是希望被捕获的异常
+        if ($throwable instanceof BusinessException) {
+            // 阻止异常冒泡
+            $this->stopPropagation();
+            //自定义异常处理
+            return $this->httpResponse->json(['msg' => $throwable->getMessage(), 'code' => $throwable->getCode(), 'data' => null])->withStatus(400);
+        }
+
+        return $response;
     }
 
     public function isValid(Throwable $throwable): bool
