feb0ebf64b58e9de30c464ac2c180693deb0d8e0
pangxiaobo <pangxiaobo@vchangyi.com>
2023-07-11 17:56:20 +0800

    logger config

--- src/config/autoload/logger.php
+++ src/config/autoload/logger.php
@@ -1,42 +1,97 @@
 <?php
 
-declare(strict_types=1);
-
-use Monolog\Handler;
-use Monolog\Formatter;
-use Monolog\Logger;
-
+declare (strict_types = 1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 return [
     'default' => [
-        'handlers' => [
-            [
-                'class' => Handler\StreamHandler::class,
-                'constructor' => [
-                    'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
-                    'level' => Logger::INFO,
-                ],
-                'formatter' => [
-                    'class' => Formatter\LineFormatter::class,
-                    'constructor' => [
-                        'format' => null,
-                        'dateFormat' => null,
-                        'allowInlineLineBreaks' => true,
-                    ],
-                ],
+        'handler'   => [
+            'class'       => Monolog\Handler\RotatingFileHandler::class,
+            'constructor' => [
+                'filename' => BASE_PATH . '/runtime/logs/hyperf.log',
+                'level'    => Monolog\Logger::DEBUG,
             ],
-            [
-                'class' => Handler\StreamHandler::class,
-                'constructor' => [
-                    'stream' => BASE_PATH . '/runtime/logs/hyperf-debug.log',
-                    'level' => Logger::DEBUG,
-                ],
-                'formatter' => [
-                    'class' => Formatter\JsonFormatter::class,
-                    'constructor' => [
-                        'batchMode' => Formatter\JsonFormatter::BATCH_MODE_JSON,
-                        'appendNewline' => true,
-                    ],
-                ],
+        ],
+        'formatter' => [
+            'class'       => Monolog\Formatter\LineFormatter::class,
+            'constructor' => [
+                'format'                => "[%datetime%]%channel%.%level_name%:%message%\n %context%",
+                'dateFormat'            => 'Y-m-d H:i:s',
+                'allowInlineLineBreaks' => true,
+            ],
+        ],
+    ],
+    'request' => [ //请求channel  在request中间件生成请求日志
+        'handler'   => [
+            'class'       => Monolog\Handler\RotatingFileHandler::class,
+            'constructor' => [
+                'filename' => BASE_PATH . '/runtime/logs/request/request.log',
+                'level'    => Monolog\Logger::INFO,
+            ],
+        ],
+        'formatter' => [
+            'class'       => Monolog\Formatter\LineFormatter::class,
+            'constructor' => [
+                'format'                => "[%datetime%]%channel%:\r\n%context%\r\n",
+                'dateFormat'            => 'Y-m-d H:i:s',
+                'allowInlineLineBreaks' => true,
+            ],
+        ],
+    ],
+    'error'   => [ //错误日志
+        'handler'   => [
+            'class'       => Monolog\Handler\StreamHandler::class,
+            'constructor' => [
+                'stream' => BASE_PATH . '/runtime/logs/error.log',
+                'level'  => Monolog\Logger::ERROR,
+            ],
+        ],
+        'formatter' => [
+            'class'       => Monolog\Formatter\LineFormatter::class,
+            'constructor' => [
+                'format'                => "[%datetime%]%level_name%:%message%\n",
+                'dateFormat'            => 'Y-m-d H:i:s',
+                'allowInlineLineBreaks' => true,
+            ],
+        ],
+    ],
+    'queue'   => [ //队列日志
+        'handler'   => [
+            'class'       => Monolog\Handler\StreamHandler::class,
+            'constructor' => [
+                'stream' => BASE_PATH . '/runtime/logs/queue.log',
+                'level'  => Monolog\Logger::DEBUG,
+            ],
+        ],
+        'formatter' => [
+            'class'       => Monolog\Formatter\LineFormatter::class,
+            'constructor' => [
+                'format'                => "[%datetime%]%level_name%:%message%\n",
+                'dateFormat'            => 'Y-m-d H:i:s',
+                'allowInlineLineBreaks' => true,
+            ],
+        ],
+    ],
+    'pay'     => [
+        'handler'   => [
+            'class'       => Monolog\Handler\RotatingFileHandler::class,
+            'constructor' => [
+                'filename' => BASE_PATH . '/runtime/logs/pay.log',
+                'level'    => Monolog\Logger::DEBUG,
+            ],
+        ],
+        'formatter' => [
+            'class'       => Monolog\Formatter\LineFormatter::class,
+            'constructor' => [
+                'format'                => "[%datetime%]%channel%.%level_name%:%message%\n %context%",
+                'dateFormat'            => 'Y-m-d H:i:s',
+                'allowInlineLineBreaks' => true,
             ],
         ],
     ],
